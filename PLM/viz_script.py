507b62da9daa35568a1915d295531cca60248f51
Andres <nanoandres_24@hotmail.com>
2022-12-17 17:42:05 +0100

    dirty viz script

--- PLM/viz_script.py
+++ PLM/viz_script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from colorama import Style, Back
 from utils import get_span
 from sklearn import metrics
@@ -92,11 +93,13 @@ def main():
     overall_mod_2 = []
     for i in range(bert_base.shape[0]):
         #pred_1 = bert_base["predictions"].iloc[i]
-        pred_1 = lstm_base["predictions"].iloc[i]
-        #pred_2 = distilbert_base["predictions"].iloc[i]
+        pred_1 = distilbert_base["predictions"].iloc[i]
+        #pred_1 = lstm_base["predictions"].iloc[i]
+        pred_2 = crf_distilbert["predictions"].iloc[i]
         #pred_2 = crf_bert["predictions"].iloc[i]
-        pred_2 = lstm_crf["predictions"].iloc[i]
-        true = lstm_base["true"].iloc[i]
+        #pred_2 = lstm_crf["predictions"].iloc[i]
+        true = distilbert_base["true"].iloc[i]
+        #true = bert_base["true"].iloc[i]
         #sent = bert_base["sent"].iloc[i]
         qual_results, mod_1_res, mod_2_res = span_counter(true, pred_1, pred_2)
         overall_mod_1.extend(mod_1_res)
@@ -108,20 +111,57 @@ def main():
                                             "m2_only": m2_only, "both_incorr": both_incorr})
     
     
-    print(poor_annotations)
     spans = pd.DataFrame.from_dict(span_sents, orient="index")
+    #spans.to_csv("BERT_and_CRF.csv")
+    #spans.to_csv("DistilBERT_and_CRF.csv")
+    #spans.to_csv("LSTM_and_CRF.csv")
     print(spans.head())
     #print(spans["both_corr"].sum())
     #print(spans["m1_only"].sum())
     #print(spans["m2_only"].sum())
     #print(spans["both_incorr"].sum())
+    # Select Confusion Matrix Size
+    results = [[spans["both_corr"].sum(), spans["m1_only"].sum()],
+               [spans["m2_only"].sum(), spans["both_incorr"].sum()]]
+    plt.figure(figsize=(10, 8))
     
-    confusion_matrix = metrics.confusion_matrix(overall_mod_1, overall_mod_2)
-    print(confusion_matrix)
-    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [False, True])
-    cm_display.plot()
-    plt.savefig("mygraph.png")
-    plt.show() 
+    # Create Confusion Matrix
+    x_axis_labels = ["Model 2 Correct", "Model 2 Incorrect"]
+    y_axis_labels = ["Model 1 Correct", "Model 2 Incorrect"]
+    sns_confusion = sns.heatmap(results, annot=True, fmt='d', xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+    #sns_confusion = sns.heatmap(results, annot=label_res, fmt='d')
+    
+    # Set the Title
+    #sns_confusion.set(title='LSTM Confusion Matrix')
+    #sns_confusion.set(title='BERT Confusion Matrix')
+    sns_confusion.set(title='DistilBERT Confusion Matrix')
+    
+    # Set the Labels
+    #sns_confusion.set(xlabel='LSTM-CRF', ylabel='LSTM Base')
+    #sns_confusion.set(xlabel='BERT-CRF', ylabel='BERT Base')
+    sns_confusion.set(xlabel='DistilBERT-CRF', ylabel='DistilBERT Base')
+    
+    # Display the Confusion Matrix
+    #plt.show()
+    #plt.savefig("LSTM Confusion Matrix")
+    #plt.savefig("BERT Confusion Matrix")
+    plt.savefig("DistilBERT Confusion Matrix")
+    
+
+    #confusion_matrix = metrics.confusion_matrix(overall_mod_1, overall_mod_2)
+    #print(confusion_matrix)
+    #cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [False, True])
+    #cm_display.plot()
+    #plt.savefig("mygraph.png")
+    #print(poor_annotations)
+    #for i in poor_annotations:
+    #    #print(bert_base["true"].iloc[i])
+    #    continue
+    print(bert_base["sent"].iloc[28])
+    print(bert_base["true"].iloc[28])
+    print(bert_base["sent"].iloc[98])
+    print(bert_base["true"].iloc[98])
+    #plt.show() 
     #for t_1, t_2 in zip(test_1, test_2):
     #for t_1, t_2 in zip(test_1, test_2):
     #    if t_1 != t_2:
